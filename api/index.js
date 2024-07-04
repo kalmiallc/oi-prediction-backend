548d29a4629d88fcffc535c838cc1f50cc3d7fdd
bterpinc <borut.terpinc@kalmia.si>
2024-07-04 16:26:32 +0200

    RPC proxy

--- api/index.js
+++ api/index.js
@@ -11,14 +11,14 @@ const version = '1.0.0'
 // Create application/x-www-form-urlencoded parser
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(express.static('public'));
+// act as get proxy for the https://coston2-api.flare.network/ext/C/rpc
+app.use('/rpc', proxy('coston2-api.flare.network/ext/C/rpc'));
 
 app.get('/', function (req, res) {
 	res.send('oi-flare-proxy-api: ' + version);
 });
 
-// act as get proxy for the https://coston2-api.flare.network/ext/C/rpc
-app.use('/RPC', proxy('https://coston2-api.flare.network/ext/C/rpc'));
+
 
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
